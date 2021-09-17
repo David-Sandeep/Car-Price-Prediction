a5769eb8977f3a4c405f88630d6720bf478bcff4
David-Sandeep <54441866+David-Sandeep@users.noreply.github.com>
2021-09-17 14:07:12 +0530

    added pkl

--- app.py
+++ app.py
@@ -8,58 +8,6 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
-url ='https://drive.google.com/file/d/1adq9EUFi-kUqg-KYa5hT7ZgrpLK6paWn/view?usp=drivesdk'
-
-
-
-
-
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)    
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-
-
-file_id = '1adq9EUFi-kUqg-KYa5hT7ZgrpLK6paWn'
-destination = 'random_forest_regression_model.pkl'
-download_file_from_google_drive(file_id, destination)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
